5fca1fc5b8834d50ad7e7a1a335cdb67216d0319
Dmitrievich Vyacheslav <ittkirsan@gmail.com>
2022-04-14 14:34:16 +0300

    добавил контроль ввода

--- цикл_for/task18.py
+++ цикл_for/task18.py
@@ -19,15 +19,23 @@ print('Введите местоположение коня: ')
 horse_x = float(input('x = '))
 horse_y = float(input('y = '))
 horse_x, horse_y = int(horse_x * 10), int(horse_y * 10)
-
 print()
+
 print('Введите местоположение точки на доске: ')
 point_x = float(input('x = '))
 point_y = float(input('y = '))
 point_x, point_y = int(point_x * 10), int(point_y * 10)
 
+while (point_x < 0) or (7 < point_x) or (point_y < 0) or (7 < point_y):
+
+    print('Вы не верно указали координату. Попробуйте еще раз!')
+    point_x = float(input('x = '))
+    point_y = float(input('y = '))
+    point_x, point_y = int(point_x * 10), int(point_y * 10)
+
 print('Конь в клетке (' + str(horse_x) + ', ' + str(horse_y) +
       '). Точка в клетке (' + str(point_x) + ', ' + str(point_y) + ')')
+
 if (abs(horse_x - point_x) == 2 and abs(horse_y - point_y) == 1) or (abs(horse_x - point_x) == 1 and abs(horse_y - point_y)) == 2:
     print('Да, конь может ходить в эту точку.')
 else:
